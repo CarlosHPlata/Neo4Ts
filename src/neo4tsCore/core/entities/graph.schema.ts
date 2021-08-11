d976f2253860c91e6df7bf777d7b6a18ade35c30
kingskull619 <carl_18_hp@hotmail.com>
2021-08-11 16:25:14 -0500

    feat: override abstraction from scheme

--- src/neo4tsCore/core/entities/graph.schema.ts
+++ src/neo4tsCore/core/entities/graph.schema.ts
@@ -1,10 +1,19 @@
 import {GraphAbstraction, GraphRelationship} from "../dtos";
 
 export class GraphSchemaEntity {
+    private readonly ALIAS_INDEX = 0; 
+    private readonly ABSTRACTION_INDEX = 1;
+
     constructor(
         private readonly fullGraphAbstraction: GraphAbstraction
     ) {}
 
+    /**
+     * It retuns a valid GraphAbstraction based on the original scheme.
+     *  
+     *  @param {string|string[]} [selectedEntities] - it will filter the schema and return only the entities you need
+     *  @param {boolean} [withRelationships=true] - it will tell the filter functionality to retrieve you the relationships for all nodes matching your selectedEntities
+     */
     getAbstraction(selectedEntities?:string|string[], withRelationships?: boolean): GraphAbstraction {
         if (selectedEntities == null || selectedEntities.length === 0)
             return this.getAbstractionCopy();
@@ -13,8 +22,35 @@ export class GraphSchemaEntity {
         return this.createNewAbstraction(entsNames, withRelationships !=null? withRelationships : true);
     }
 
+    /**
+     * It retuns a valid GraphAbstraction using a new set of properties, it will find for the original scheme and override the shceme with your properties defined on newSetOFAbstraction.
+     *  
+     *  @param {GraphAbstraction} newSetOfAbstraction - used to override and add properties to the original schema
+     *  @param {string|string[]} [selectedEntities] - it will filter the schema and return only the entities you need
+     *  @param {boolean} [withRelationships=true] - it will tell the filter functionality to retrieve you the relationships for all nodes matching your selectedEntities
+     */
+    getAbstractionOverride(newSetOfAbstraction: GraphAbstraction, selectedEntities?:string|string[], withRelationships?: boolean): GraphAbstraction {
+        const newAbstraction = this.getAbstraction(selectedEntities, withRelationships);
+
+        return this.mergeAbstraciton(newAbstraction, newSetOfAbstraction);
+    }
+
+    private mergeAbstraciton(originalAbstraction: GraphAbstraction, newStateAbstraction:GraphAbstraction): GraphAbstraction {
+        const newAbs: GraphAbstraction = {};
+        const originalEntries = Object.entries(originalAbstraction);
+        const newStateEntries = Object.entries(newStateAbstraction);
+
+        for (const originalEntry of originalEntries) {
+            const newStateEntry = newStateEntries.find(entry => entry[this.ALIAS_INDEX] === originalEntry[this.ALIAS_INDEX]) || [originalEntry[this.ALIAS_INDEX], {}];
+
+            newAbs[originalEntry[this.ALIAS_INDEX]] = { ...originalEntry[this.ABSTRACTION_INDEX], ...newStateEntry[this.ABSTRACTION_INDEX]  };
+        }
+
+        return newAbs;
+    }
+
     private getAbstractionCopy(): GraphAbstraction {
-        return { ...this.fullGraphAbstraction };
+        return JSON.parse( JSON.stringify(this.fullGraphAbstraction) );
     }
 
     private createNewAbstraction(selectedEntities: string[], withRelationships: boolean): GraphAbstraction {
@@ -33,7 +69,6 @@ export class GraphSchemaEntity {
 
         return abstraction;
     }
-
 
     private getRelationshipNames(selectedEntities: string[]) {
         const names = this.getOnlyNodeNames(selectedEntities);
